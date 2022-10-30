feeaa4974dd1acc2d4b3c4be7b789fed09faf295
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-30 18:39:36 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div>
-     <h2>Hello from React</h2>
+     <h1>Hello from React11</h1>
     </div>
   );
 }
